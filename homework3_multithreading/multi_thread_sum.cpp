df134c3b06854f88add658832cacb4979bec9995
Dmitri Glusco <dima2492@gmail.com>
2019-06-03 22:18:50 +0300

    Fixed warnings

--- homework3_multithreading/multi_thread_sum.cpp
+++ homework3_multithreading/multi_thread_sum.cpp
@@ -22,7 +22,7 @@ BMPInfo readBMP(const char* filename)
     int i;
     FILE* f = fopen(filename, "rb");
     unsigned char info[54];
-    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+    auto a = fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
 
     // extract image height and width from header
     int width = *(int*)&info[18];
@@ -30,7 +30,7 @@ BMPInfo readBMP(const char* filename)
 
     int size = 3 * width * height;
     unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
-    fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
+    a = fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
     fclose(f);
 
     for(i = 0; i < size; i += 3)
